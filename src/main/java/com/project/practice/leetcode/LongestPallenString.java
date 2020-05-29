294f4b0c1adf748bdc9a57a53800ae8ba197c668
ishangrover-9654 <49878747+ishangrover-9654@users.noreply.github.com>
2020-05-29 09:06:23 +0530

    Longest Pallen string and length of longest two distinct

--- src/main/java/com/project/practice/leetcode/LongestPallenString.java
+++ src/main/java/com/project/practice/leetcode/LongestPallenString.java
@@ -6,11 +6,13 @@ public class LongestPallenString {
 
 
     public static void main(String[] args) {
-        System.out.println(longestPallen("aba"));
+        System.out.println(longestPallen("a"));
     }
 
     public static String longestPallen(String s){
         int sLen = s.length();
+        if (s == null || s.length() < 1) return "";
+        if (s.length()==1) return s;
         for (int start = 0; start < sLen - 1; start++) {
             expand(s, start, start);
             expand(s, start, start + 1);
